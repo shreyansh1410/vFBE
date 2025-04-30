c9ee687c44bde3b65524b38ed4ba10eeb79bc8b4
Shreyansh Shukla <shreyansh.14010@gmail.com>
2025-04-30 18:37:41 +0530

    new backend

--- dist/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: "Internal server error" });
-};
